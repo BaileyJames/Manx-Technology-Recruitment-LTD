9bf8bd06f3efd09a28cb63da11ef8a0d08ee0158
BaileyJames <baileyheyworth@gmail.com>
2024-05-04 14:54:46 +0100

    Add basic, currently unencrypted register and login functionality

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
 require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -15,12 +19,41 @@ async function connect() {
         await client.connect();
         await client.db("Recruitment").command({ ping: 1 });
         console.log("Successful ping, connected to the database");
-    } finally {
-        await client.close();
+    } catch (e) {
+        console.error(e);
     }
 }
 
-connect().catch(console.error);
+connect();
+
+app.post("/register", async (req, res) => {
+    let collection = await client.db("Recruitment").collection("users");
+    let email = req.body.email;
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let existingUser = await collection.findOne({ email: email });
+    if (existingUser) {
+        res.send("User with email " + email + " already exists");
+        return;
+    }
+
+    let newUser = await collection.insertOne({ email: email, username: username, password: password });
+    res.send("Successfully registered user " + username);
+});
+
+app.post("/login", async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let user = await client.db("Recruitment").collection("users").findOne({ email: email, password: password });
+    
+    if (user) {
+        res.send("Successfully logged in as " + user.username);
+        return;
+    }
+    res.send("Invalid email or password");
+});
 
 app.get("/", (req, res) => {
     res.send("Hello");
