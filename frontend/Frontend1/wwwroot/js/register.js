d9e2e801d0ba4a9768ff3f7b6863213d3f8c139e
Oliver Harrison <oliverctharrison@gmail.com>
2024-05-10 12:04:01 +0100

    Added register functionality

---
+++ frontend/Frontend1/wwwroot/js/register.js
@@ -0,0 +1,9 @@
+﻿document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    var password1 = document.getElementById("password1").value;
+    var password2 = document.getElementById("password2").value;
+
+    if (password1 !== password2) {
+        alert("Passwords do not match.");
+        event.preventDefault(); 
+    }
+});
