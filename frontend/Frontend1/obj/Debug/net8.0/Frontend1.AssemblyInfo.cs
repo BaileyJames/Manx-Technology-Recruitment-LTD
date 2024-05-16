c6e118a0dd90f3fad674ff0112b501494e764642
Oliver Harrison <oliverctharrison@gmail.com>
2024-05-16 15:23:04 +0100

    fixed footer, navbar and jobs public page and admin page.

--- frontend/Frontend1/obj/Debug/net8.0/Frontend1.AssemblyInfo.cs
+++ frontend/Frontend1/obj/Debug/net8.0/Frontend1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ca5572c5bcbcaf3dbb9eb14f26308211f8ecb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cd5ece33e2e59e54195ac7218b624c54454c77d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
